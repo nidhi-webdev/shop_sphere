09c8020c16d3da599df6a79eccdbe002d5090e20
nidhi <kumarinidhi0594@gmail.com>
2025-08-17 14:42:20 +0200

    assets file created

--- Shop-Sphere/src/app/app.routes.ts
+++ Shop-Sphere/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { ProductComponent } from './Components/product-component/product-component';
+import { LoginComponent } from './Components/login-component/login-component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/products', pathMatch: 'full' },
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+     { path: 'login', component: LoginComponent },
+
     { path: 'products', component: ProductComponent }
 ];
