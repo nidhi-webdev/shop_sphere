91bfb17f341d0fb1d9950fe786b8aef8ff462244
nidhi <kumarinidhi0594@gmail.com>
2025-08-18 17:01:19 +0200

    Formated

--- Shop-Sphere/src/app/Models/products.model.ts
+++ Shop-Sphere/src/app/Models/products.model.ts
@@ -1,16 +1,16 @@
 export interface PRODUCTS {
-    id?: number,
-    title: string,
-    price: number,
-    description: string,
-    category: string,
-    image: string,
-    rating: Rating
+  id?: number,
+  title: string,
+  price: number,
+  description: string,
+  category: string,
+  image: string,
+  rating: Rating
 }
 
 export interface Rating {
-    rate: number,
-    count: number
+  rate: number,
+  count: number
 }
 
 export interface CARTPRODUCT {
