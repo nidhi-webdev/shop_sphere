653e384819c270484b7e9563a7f8cf2fc2146c04
nidhi <kumarinidhi0594@gmail.com>
2025-08-26 16:29:03 +0200

    Shopping Cart Changed

--- Shop-Sphere/src/app/Components/shopping-basket-component/shopping-basket-component.ts
+++ Shop-Sphere/src/app/Components/shopping-basket-component/shopping-basket-component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, inject } from '@angular/core';
 import { CartService } from '../../Services/cart-service';
 import { PRODUCTS } from '../../Models/products.model';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -11,5 +12,23 @@ import { PRODUCTS } from '../../Models/products.model';
 })
 export class ShoppingBasketComponent {
   cartservice = inject(CartService);
+  route = inject(Router)
   cartItem = this.cartservice._cartItem;
+  userEmail: string = '';
+
+  ngOnInit() {
+    if (typeof window !== 'undefined') {
+      const user = localStorage.getItem('loginUser');
+      if (user) {
+        const userObj = JSON.parse(user);
+        this.userEmail = userObj.email || '';
+      }
+    }
+
+  }
+
+  logOut() {
+    this.route.navigate(['/login']);
+    localStorage.removeItem('loginUser');
+  }
 }
