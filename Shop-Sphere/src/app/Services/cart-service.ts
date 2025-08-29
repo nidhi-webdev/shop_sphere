dac85ce979996a77c89263ce84094fd0da1274d9
nidhi <kumarinidhi0594@gmail.com>
2025-08-29 16:09:46 +0200

    Formated1

--- Shop-Sphere/src/app/Services/cart-service.ts
+++ Shop-Sphere/src/app/Services/cart-service.ts
@@ -8,22 +8,22 @@ export class CartService {
   public _cartItem = signal<CARTPRODUCT[]>(this.loadCartFromStorage());
 
   addToCart(product: PRODUCTS) {
-   const items = this._cartItem();
-  const existing = items.find(item => item.product.id === product.id);
+    const items = this._cartItem();
+    const existing = items.find(item => item.product.id === product.id);
 
-  if(existing) {
-    this._cartItem.update(items => 
-      items.map(item => 
-        item.product.id === product.id ? 
-        { ...item, quantity: item.quantity + 1} : item
-      )
-    );
-  } else {
-    this._cartItem.update(items => 
-    [ ...items, {product, quantity: 1}]
-    );
-  }
-  this.saveCartToStorage();
+    if (existing) {
+      this._cartItem.update(items =>
+        items.map(item =>
+          item.product.id === product.id ?
+            { ...item, quantity: item.quantity + 1 } : item
+        )
+      );
+    } else {
+      this._cartItem.update(items =>
+        [...items, { product, quantity: 1 }]
+      );
+    }
+    this.saveCartToStorage();
   }
 
   saveCartToStorage() {
@@ -32,24 +32,24 @@ export class CartService {
 
   loadCartFromStorage(): CARTPRODUCT[] {
     const data = localStorage.getItem('cart');
-      
+
     const parsed = data ? JSON.parse(data) as CARTPRODUCT[] : [];
-      console.log('Cart in localStorage (parsed):', parsed);
-     return parsed;
+    console.log('Cart in localStorage (parsed):', parsed);
+    return parsed;
   }
 
-   deleteProduct(id?: number) {
+  deleteProduct(id?: number) {
     this._cartItem.update(items => items.filter(item => item.product.id !== id));
     this.saveCartToStorage();
   }
 
-increaseProduct(product: PRODUCTS ) {
-  this._cartItem.update(items => items.map(item => 
-    item.product.id === product.id
-      ? { ...item, quantity: (item.quantity || 1) + 1 }
-      : item
-  ));
-  this.saveCartToStorage();
-}
+  increaseProduct(product: PRODUCTS) {
+    this._cartItem.update(items => items.map(item =>
+      item.product.id === product.id
+        ? { ...item, quantity: (item.quantity || 1) + 1 }
+        : item
+    ));
+    this.saveCartToStorage();
+  }
 
 }
